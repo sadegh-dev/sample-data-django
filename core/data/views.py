29a88a9b654b75a7089cc08a4058083d434c4638
sadegh-dev <sadegh.rez@gmail.com>
2022-07-28 16:40:04 +0430

    #develop field-lookups

--- core/data/views.py
+++ core/data/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from .models import DataEn
+from django.db.models import F
 
-
+"""
 def index(request):
     data = DataEn.objects.all()
+    context = {
+        'data':data ,
+    }
+    return render(request,'data/index.html',context)
+"""
+
+def index(request):
+    data = DataEn.objects.filter(id =1) 
     context = {
         'data':data ,
     }
